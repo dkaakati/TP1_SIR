4643cf668b57d95a12aa09e87a51587c2131880e
dkaakati <dkaakatidania.kaakati@gmail.com>
2016-01-06 18:25:56 +0100

    first commit

---
+++ maven2/src/main/java/org/apache/maven/maven2/App.java
@@ -0,0 +1,23 @@
+package org.apache.maven.maven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * unn com
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        if(true){
+        	System.out.println("true");
+        }
+        else{
+        	System.out.println("false");
+        }
+    }
+}
